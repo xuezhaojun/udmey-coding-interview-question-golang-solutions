8ab23f4f526ff6eaf75d20e2b2236e950db37c9e
xuezhaojun <1340762527@qq.com>
2018-12-04 13:45:43 +0800

    add code

---
+++ 02_palindrome/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	str := "hello"
+	str1 := "abba"
+	str2 := "世界界世"
+	fmt.Println(check(str))
+	fmt.Println(check(str1))
+	fmt.Println(check(str2))
+}
+
+// check 用于检验字符串是否满足回文的要求
+func check(str string) bool {
+	runes := []rune(str)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		if runes[i] != runes[j] {
+			return false
+		}
+	}
+	return true
+}

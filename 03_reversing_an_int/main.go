a89e44f6086cb3f19bd9ebb074378f8b52a9fa9e
xuezhaojun <1340762527@qq.com>
2018-12-07 13:40:40 +0800

    add code reverse int solution2

--- 03_reversing_an_int/main.go
+++ 03_reversing_an_int/main.go
@@ -6,22 +6,22 @@ import (
 )
 
 func main() {
-	fmt.Println(P(100))
-	fmt.Println(P(101))
 	fmt.Println(P(10032))
 	fmt.Println(P(-11003223001))
 	fmt.Println(P(-90))
+
+	fmt.Println(P2(10032))
+	fmt.Println(P2(-11003223001))
+	fmt.Println(P2(-90))
 }
 
-// P 是否回文
 // 转化为字符串后，通过字符串的回文判断方法
 func P(num int) int {
 	str := strconv.Itoa(num)
 	runes := []rune(str)
 
-	// 去掉正负号
-	pre := runes[0]
-	if pre == '-' {
+	// 去掉负号
+	if num < 0 {
 		runes = runes[1:]
 	}
 
@@ -31,9 +31,21 @@ func P(num int) int {
 	}
 	newNum, _ := strconv.Atoi(string(runes))
 
-	// 添加正负号
-	if pre == '-' {
+	// 添加负号
+	if num < 0 {
 		newNum = -newNum
 	}
 	return newNum
+}
+
+// 直接对不同的位数进行判断
+func P2(num int) (newNum int) {
+	for num != 0 {
+		temp := newNum*10 + num%10
+		if temp/10 != newNum {
+			return 0 // 溢出
+		}
+		newNum, num = temp, num/10
+	}
+	return
 }

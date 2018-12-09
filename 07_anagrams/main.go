3ac2e416666bc4a85996df0496c00012ebfe2a09
xuezhao <1340762527@qq.com>
2018-12-09 15:18:57 +0800

    add main_test

--- 07_anagrams/main.go
+++ 07_anagrams/main.go
@@ -3,6 +3,29 @@ package main
 // anagram 是指，两个字符串中，如果字符的数量相同，则是 anagram 如果不同，则不是
 // 对应leetCode 242
 
-func anagram(str1, str2 string) (result bool) {
+// 使用map对str1和str2中的不同字节进行计数
+// 对 str1 进行加
+// 对 str2 进行减
+func anagram(s, t string) (result bool) {
+	m := make(map[rune]int)
+	runes1 := []rune(s)
+	runes2 := []rune(t)
 
+	if len(runes1) != len(runes2) {
+		return false
+	}
+
+	length := len(runes1)
+	for i := 0; i < length; i++ {
+		m[runes1[i]] += 1
+		m[runes2[i]] -= 1
+	}
+
+	for _, c := range m {
+		if c != 0 {
+			return false
+		}
+	}
+
+	return true
 }
